49ba6c2422f0e4ab0fa4177ddb9349ad9261ea44
Vlad <boogovo@gmail.com>
2024-06-28 20:16:46 +0300

    Initial Commit

---
+++ JustChat/JustChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustChatApp.swift
+//  JustChat
+//
+//  Created by Vlad on 28/6/24.
+//
+
+import SwiftUI
+
+@main
+struct JustChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
