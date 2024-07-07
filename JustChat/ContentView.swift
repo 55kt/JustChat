c652ad580104180281df000f0b18fd734f70f836
System Administrator <root@VLADYSLAVs-Mac-mini.local>
2024-07-07 10:01:49 +0300

    Terminal Commit

--- JustChat/ContentView.swift
+++ JustChat/ContentView.swift
@@ -27,7 +27,7 @@ struct ContentView: View {
                         
                         CustomTabView(tabSelection: $selectedTab)
                     }
-                    Text("Test")
+                    Text("Test2")
                 }
             }
             
